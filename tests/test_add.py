6ba6e6441a508837bfeb91c0bf4aa918f87c21d1
shiyi0113 <shiyileshengxia@gmail.com>
2026-04-30 07:00:41 +0800

    wip...

--- tests/test_add.py
+++ tests/test_add.py
@@ -2,18 +2,25 @@ import torch
 import ofk 
 
 def test_my_cuda_add():
-    print("🚀 开始测试自定义 CUDA 算子...")
+    print("🚀 开始测试...")
     
     size = (1024, 1024)
     a = torch.randn(size, dtype=torch.float32, device='cuda')
     b = torch.randn(size, dtype=torch.float32, device='cuda')
+    # warmup
+    for _ in range(10):
+        torch.ops.my_lib.add(a, b) # 这里的 my_lib 和 add 对应你在 entry.cc 里写的 TORCH_LIBRARY(my_lib, m) { m.def("add...") }
+    torch.cuda.synchronize()
     
-    # 这里的 my_lib 和 add 对应你在 entry.cc 里写的 TORCH_LIBRARY(my_lib, m) { m.def("add...") }
+    torch.cuda.cudart().cudaProfilerStart()   # ← profile 从这里开始
     my_output = torch.ops.my_lib.add(a, b)
+    torch.cuda.synchronize()
     torch_output = a + b
+    torch.cuda.cudart().cudaProfilerStop()    # ← profile 到这里结束
+    
     torch.testing.assert_close(my_output, torch_output)
     
-    print("✅ 测试完美通过！你的自定义 CUDA 算子计算结果与 PyTorch 原生算子完全一致！")
+    print("✅ 测试通过！")
 
 if __name__ == "__main__":
     test_my_cuda_add()
